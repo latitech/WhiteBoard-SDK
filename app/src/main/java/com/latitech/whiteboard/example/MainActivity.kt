7a1765064902068ab8f4256480650ead81dbd0af
超悟空 <39251474@163.com>
2021-06-24 17:31:45 +0800

    增加测试服务器用户

--- app/src/main/java/com/latitech/whiteboard/example/MainActivity.kt
+++ app/src/main/java/com/latitech/whiteboard/example/MainActivity.kt
@@ -10,13 +10,25 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
 
+        // 正式服务器测试号
+
+//        private const val APP_ID = "a4b26ecae3744e3fb60ff679e186cd98"
+//
+//        private const val ROOM_ID = "399bbd57486b4a83b68485f353dd7153"
+//
+//        private const val USER_ID = "0457987b-82f7-409d-8d58-24b42dda76f2"
+//
+//        private const val TOKEN = "c0c9b8a3119190152dd5e6697eb78d6a"
+
+        // 测试服务器测试号
+
         private const val APP_ID = "a4b26ecae3744e3fb60ff679e186cd98"
 
-        private const val ROOM_ID = "399bbd57486b4a83b68485f353dd7153"
+        private const val ROOM_ID = "d8471edb7b364744941f60eba4df2887"
 
-        private const val USER_ID = "0457987b-82f7-409d-8d58-24b42dda76f2"
+        private const val USER_ID = "test"
 
-        private const val TOKEN = "c0c9b8a3119190152dd5e6697eb78d6a"
+        private const val TOKEN = "c63987b65858dcdf38a7611d9c6fcd77"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
