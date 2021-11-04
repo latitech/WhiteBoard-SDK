4a67ebb9fa6b9679bd91aaf0a35d0bee7639a49e
超悟空 <39251474@163.com>
2021-11-04 16:26:49 +0800

    增加闪屏页

--- app/src/main/java/com/latitech/whiteboard/example/MainActivity.kt
+++ app/src/main/java/com/latitech/whiteboard/example/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.latitech.whiteboard.example
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 import com.latitech.whiteboard.example.databinding.ActivityMainBinding
 import com.latitech.whiteboard.model.JoinConfig
 import splitties.activities.start
@@ -32,10 +37,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_WhiteBoardSDK)
         super.onCreate(savedInstanceState)
 
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        splashTransition()
+
         binding.join.setOnClickListener {
             // 加入房间
             val params = JoinConfig(
@@ -48,6 +57,44 @@ class MainActivity : AppCompatActivity() {
             start<RoomActivity> {
                 putExtra(RoomActivity.ROOM_DATA_TAG, params)
             }
+        }
+    }
+
+    /**
+     * 闪屏页转换
+     */
+    private fun splashTransition() {
+        ImageView(this).apply {
+            scaleType = ImageView.ScaleType.FIT_XY
+            setImageDrawable(
+                ResourcesCompat.getDrawable(
+                    resources,
+                    R.drawable.launch_background,
+                    theme
+                )
+            )
+            addContentView(
+                this,
+                ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            )
+
+            animate()
+                .alpha(0.0f)
+                .setDuration(1000)
+                .setListener(
+                    object : AnimatorListenerAdapter() {
+                        override fun onAnimationCancel(animation: Animator) {
+                            onAnimationEnd(animation)
+                        }
+
+                        override fun onAnimationEnd(animation: Animator) {
+
+                            (parent as ViewGroup).removeView(this@apply)
+                        }
+                    })
         }
     }
 }
