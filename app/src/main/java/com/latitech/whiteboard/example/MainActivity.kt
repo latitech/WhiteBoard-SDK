0d4969738e3fce2c67ec39c51d65f694c0be81bd
超悟空 <39251474@163.com>
2022-11-23 16:04:38 +0800

    增加房间和白板状态显示

--- app/src/main/java/com/latitech/whiteboard/example/MainActivity.kt
+++ app/src/main/java/com/latitech/whiteboard/example/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         /**
          * 申请的SDK appId
          */
-        private const val APP_ID = "8f31ad0d-79ac-46b3-8958-7c934a83f167"
+        private const val APP_ID = "a4b26ecae3744e3fb60ff679e186cd98"
 
         /**
          * 在房间中使用的用户id
@@ -26,27 +26,27 @@ class MainActivity : AppCompatActivity() {
         /**
          * 加入时的房间用户token
          */
-        private const val TOKEN = "ef5388b7ef0f298f8c5916ca53a823fd"
+        private const val TOKEN = "3f808f3953cad85efca86a4c398b9a12"
 
         /**
          * 加入的房间id
          */
-        private const val ROOM_ID = "0009f86c56c9442ba2a463e76b55f044"
+        private const val ROOM_ID = "b1b4c08a254a4488b15749df23e27356"
 
         /**
          * 房间中已经创建的白板bucketId
          * 如果房间只有自带的默认白板，没有添加更多白板或不需要体验白板切换功能，则可以留空
          */
         private val BUCKET_IDS = arrayOf(
-            "8c886ada-e40b-4be0-b6ae-1a239c847f12",
-            "44300bc0-4f3e-4664-bd2f-9a6bfe666c13",
-            "7d3ed69b-4354-40d9-8571-ce54d8d73527",
+            "76f5e1b7-2bc4-449a-86e3-4662f536b716", // 普通白板
+            "59456d75-e35d-4e6b-84aa-f1d2d441cf7f", // pdf模式
+            "b1e269ff-a229-4a30-9596-33d8ee63785c", // ppd模式
         )
 
         /**
          * 查看回放的录制id
          */
-        private val RECORD_ID = "0ee04e76-81df-48ca-8e9d-cd854c0ce77d"
+        private val RECORD_ID = "c1d8ef70-3951-402f-bc33-cf10d5ad604e"
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
